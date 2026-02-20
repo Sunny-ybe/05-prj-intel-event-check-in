ef5b3515bec2d1ddaf09c021de52b2c293dd2dae
Sashikant Kumar <146484950+Sunny-ybe@users.noreply.github.com>
2026-02-20 04:36:12 +0000

    Implement form submission handling for check-in process

---
+++ script.js
@@ -0,0 +1,34 @@
+// Get needed DOM elements
+
+const form = document.getElementById("checkInForm");
+const teamSelect = document.getElementById("teamSelect");
+const nameInput = document.getElementById("attendeeName");
+
+// Handle Form Submission
+form.addEventListener('submit', function(event){
+  event.preventDefault(); // prevent from refreshing the page once submission hits
+  const name = nameInput.value;
+  const team = teamSelect.value;
+
+  const teamName = teamSelect.selectedOptions[0].text; // Get the text of the selected option
+  console.log(name, teamName);
+  //Increment count
+  count++;
+  console.log("Total Check Ins: "+ count);
+
+  // Update Progress Bar
+  const percent = Math.round(count * 100/ countMax)  + "%";
+  console.log("Progress: "+ percent);
+
+  const teamCounter = document.getElementById(team + 'Count');
+  teamCounter.textContent = parseInt(teamCounter.textContent)+1;
+
+  // show welcome message
+  const message = 'Welcome ' + name + ' from team ' + teamName;
+  console.log(message);
+  form.reset();
+});
+// Track attendence 
+
+let count = 0;
+const countMax = 50;
